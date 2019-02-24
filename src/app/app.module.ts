de1b5cc6298f5b5ed6c6e72d61a8f122f3c2d8d5
Adam <bagrowski.adam@gmail.com>
2019-02-24 11:58:10 +0100

    SharedModule, MenuComponent, BackgroundColorDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackgroundColorDirective } from './background-color.directive';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundColorDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
