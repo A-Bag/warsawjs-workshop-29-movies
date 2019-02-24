1394c8d5332caf119a126d618152c69612768940
Adam <bagrowski.adam@gmail.com>
2019-02-24 17:52:59 +0100

    Search movies

--- src/app/logic/components/page-video-list/page-video-list.component.ts
+++ src/app/logic/components/page-video-list/page-video-list.component.ts
@@ -10,17 +10,31 @@ import { Movies } from '../../interfaces/movies.interface';
 export class PageVideoListComponent implements OnInit {
 
   public movies: Movies = [];
+  public foundMovies: Movies = [];
 
   constructor(private moviesService: MoviesService) { }
 
   async ngOnInit() {
+    this.getAllMovies();
+  }
+
+  async getAllMovies() {
     this.movies = await this.moviesService.fetchMovies();
   }
 
-  async getMovies() {
+  async logMovies() {
     const response = await this.moviesService.fetchMovies();
     console.log(response);
     return response;
+  }
+
+  search(event) {
+    const searchTerm = event.target.value.toLowerCase();
+    const foundMovies = this.movies.filter( movie =>  {
+      const string = movie.title + movie.description;
+      return string.toLowerCase().includes(searchTerm);
+    });
+    this.foundMovies = foundMovies;
   }
 
 }
