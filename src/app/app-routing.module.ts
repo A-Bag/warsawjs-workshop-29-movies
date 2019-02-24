36c64c776e8b583c652edb21e8a34341e52e9324
Adam <bagrowski.adam@gmail.com>
2019-02-24 12:22:23 +0100

    Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PageHomeComponent } from './logic/components/page-home/page-home.component';
+import { PageContactComponent } from './logic/components/page-contact/page-contact.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PageHomeComponent
+  },
+  {
+    path: 'contact',
+    component: PageContactComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BackgroundColorDirective } from './background-color.directive';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { LogicModule } from './logic/logic.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,8 +14,11 @@ import { SharedModule } from './shared/shared.module';
     BackgroundColorDirective
   ],
   imports: [
+    RouterModule,
+    AppRoutingModule,
+    LogicModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
